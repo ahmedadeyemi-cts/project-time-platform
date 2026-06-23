e634d2edef30c762ba3a518c4f783fbe5a5c05da
ahmedadeyemi-cts <ahmedadeyemi@gmail.com>
2026-06-23 14:28:23 -0700

    Render manager approval panel

--- src/frontend/project-time-web/src/main.jsx
+++ src/frontend/project-time-web/src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import ManagerApprovalPanel from './ManagerApprovalPanel.jsx';
 import HelpAssistant from './HelpAssistant.jsx';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ManagerApprovalPanel />
     <HelpAssistant />
   </React.StrictMode>
 );
