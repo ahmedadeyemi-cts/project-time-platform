59040eebbf5dd633c1ebd48a06aa758aa96567f4
ahmedadeyemi-cts <ahmedadeyemi@gmail.com>
2026-06-23 14:58:31 -0700

    Render approval notification banner

--- src/frontend/project-time-web/src/main.jsx
+++ src/frontend/project-time-web/src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import ManagerApprovalPanel from './ManagerApprovalPanel.jsx';
+import ApprovalNotificationBanner from './ApprovalNotificationBanner.jsx';
 import HelpAssistant from './HelpAssistant.jsx';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ApprovalNotificationBanner />
     <ManagerApprovalPanel />
     <HelpAssistant />
   </React.StrictMode>
