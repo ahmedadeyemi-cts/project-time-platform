1e3a4ad93396caa1b2ee3da78baef096dc00fc97
ahmedadeyemi-cts <ahmedadeyemi@gmail.com>
2026-06-23 13:16:24 -0700

    Add rollback for daily timesheet status table

---
+++ database/rollback/007_timesheet_day_submission_status_rollback.sql
@@ -0,0 +1,13 @@
+-- Project Pulse
+-- Rollback: 007_timesheet_day_submission_status_rollback.sql
+
+BEGIN;
+
+DROP INDEX IF EXISTS idx_timesheet_day_status_timesheet_status;
+DROP INDEX IF EXISTS idx_timesheet_day_status_user_date;
+DROP TABLE IF EXISTS timesheet_day_statuses;
+
+DELETE FROM schema_migrations
+WHERE migration_id = '007_timesheet_day_submission_status';
+
+COMMIT;
