0977211ef50600702f3869d308f05b746234ad0c
ahmedadeyemi-cts <ahmedadeyemi@gmail.com>
2026-06-23 12:29:52 -0700

    Render contextual help assistant globally

--- src/frontend/project-time-web/src/main.jsx
+++ src/frontend/project-time-web/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import HelpAssistant from './HelpAssistant.jsx';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <HelpAssistant />
   </React.StrictMode>
 );
