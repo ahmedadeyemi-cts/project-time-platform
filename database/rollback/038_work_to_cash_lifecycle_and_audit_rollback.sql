77e802785ded0c684c633e03e14e53e1a3c77023
ahmedadeyemi-cts <ahmedadeyemi@gmail.com>
2026-07-23 04:03:08 -0700

    chore: roll back governed billing evidence

--- database/rollback/038_work_to_cash_lifecycle_and_audit_rollback.sql
+++ database/rollback/038_work_to_cash_lifecycle_and_audit_rollback.sql
@@ -6,6 +6,8 @@ DROP TRIGGER IF EXISTS trg_projectpulse038_invoice_reactivation
     ON billing_invoices;
 DROP TRIGGER IF EXISTS trg_projectpulse038_live_time_entry_line
     ON billing_invoice_lines;
+DROP TRIGGER IF EXISTS trg_projectpulse038_live_readiness_line
+    ON billing_invoice_lines;
 DROP TRIGGER IF EXISTS trg_projectpulse038_invoice_audit
     ON billing_invoice_events;
 DROP TRIGGER IF EXISTS trg_projectpulse038_work_register_audit
@@ -18,8 +20,11 @@ DROP FUNCTION IF EXISTS projectpulse038_capture_work_register_change();
 DROP FUNCTION IF EXISTS projectpulse038_reject_audit_mutation();
 DROP FUNCTION IF EXISTS projectpulse038_guard_invoice_reactivation();
 DROP FUNCTION IF EXISTS projectpulse038_guard_live_time_entry_line();
+DROP FUNCTION IF EXISTS projectpulse038_guard_live_readiness_line();
 
 DROP INDEX IF EXISTS uq_billing_invoice_lines_invoice_time_entry;
+DROP INDEX IF EXISTS uq_billing_invoice_lines_invoice_readiness_review;
+DROP INDEX IF EXISTS idx_billing_invoice_lines_readiness_review;
 
 DO $$
 BEGIN
@@ -40,6 +45,9 @@ $$;
 CREATE UNIQUE INDEX IF NOT EXISTS uq_billing_invoice_lines_time_entry
     ON billing_invoice_lines(time_entry_id)
     WHERE time_entry_id IS NOT NULL;
+
+ALTER TABLE billing_invoice_lines
+    DROP COLUMN IF EXISTS billing_readiness_review_id;
 
 DROP TABLE IF EXISTS work_lifecycle_audit_events;
 DROP TABLE IF EXISTS work_closeout_records;
