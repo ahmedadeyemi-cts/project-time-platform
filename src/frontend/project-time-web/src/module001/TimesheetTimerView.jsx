b7fee946a43ecd007d6cd713f420613be21c05da
ahmedadeyemi-cts <ahmedadeyemi@gmail.com>
2026-07-24 12:12:36 -0700

    Permit deterministic UUIDs in timer selection

--- src/frontend/project-time-web/src/module001/TimesheetTimerView.jsx
+++ src/frontend/project-time-web/src/module001/TimesheetTimerView.jsx
@@ -4,7 +4,7 @@ import { calculateTimerDuration, formatElapsedSeconds } from './timesheet-durati
 import './timesheet-prep.css';
 import './timesheet-timer-recovery.css';
 
-const TIMER_TARGET_PATTERN = /^(assignment|category):[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+const TIMER_TARGET_PATTERN = /^(assignment|category):[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12}$/i;
 
 export default function TimesheetTimerView({
   targets = [], history = [], activeTimer = null, selectedTargetValue = '',
