31e92e0ace49b764ed3a7f88bb76746cc24498b6
ahmedadeyemi-cts <ahmedadeyemi@gmail.com>
2026-08-01 14:04:13 -0700

    Add Analytics Center enterprise experience rollback

---
+++ database/rollback/060_analytics_center_enterprise_experience_rollback.sql
@@ -0,0 +1,46 @@
+-- Roll back Module 030 Analytics Center enterprise experience migration 060.
+
+BEGIN;
+
+DELETE FROM app_role_permissions
+WHERE app_permission_id IN (
+    SELECT app_permission_id
+    FROM app_permissions
+    WHERE permission_code IN (
+        'VIEW_ANALYTICS_DASHBOARDS',
+        'VIEW_ANALYTICS_SCHEDULES',
+        'MANAGE_ANALYTICS_SCHEDULES',
+        'DELIVER_ANALYTICS_SCHEDULES'
+    )
+);
+
+DELETE FROM app_permissions
+WHERE permission_code IN (
+    'VIEW_ANALYTICS_DASHBOARDS',
+    'VIEW_ANALYTICS_SCHEDULES',
+    'MANAGE_ANALYTICS_SCHEDULES',
+    'DELIVER_ANALYTICS_SCHEDULES'
+);
+
+DROP TRIGGER IF EXISTS trg_projectpulse060_schedule_delivery_immutable
+    ON analytics_report_schedule_delivery_attempts;
+DROP TRIGGER IF EXISTS trg_projectpulse060_schedule_runs_immutable
+    ON analytics_report_schedule_runs;
+DROP FUNCTION IF EXISTS projectpulse060_block_analytics_schedule_evidence_mutation();
+
+DROP TABLE IF EXISTS analytics_report_schedule_delivery_attempts;
+DROP TABLE IF EXISTS analytics_report_schedule_runs;
+DROP TABLE IF EXISTS analytics_report_schedule_recipients;
+DROP TABLE IF EXISTS analytics_report_schedules;
+DROP TABLE IF EXISTS analytics_user_report_activity;
+
+ALTER TABLE enterprise_report_exports
+    DROP CONSTRAINT IF EXISTS enterprise_report_exports_export_format_check;
+ALTER TABLE enterprise_report_exports
+    ADD CONSTRAINT enterprise_report_exports_export_format_check
+    CHECK (export_format IN ('csv', 'xlsx', 'json'));
+
+DELETE FROM schema_migrations
+WHERE migration_id = '060_analytics_center_enterprise_experience';
+
+COMMIT;
