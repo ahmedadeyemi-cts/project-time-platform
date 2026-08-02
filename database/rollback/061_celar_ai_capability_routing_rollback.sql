67fb41ad07e057efefa34eedb938bb4f82b86a29
ahmedadeyemi-cts <ahmedadeyemi@gmail.com>
2026-08-02 09:30:54 -0700

    Fix migration 061 rollback registration cleanup

--- database/rollback/061_celar_ai_capability_routing_rollback.sql
+++ database/rollback/061_celar_ai_capability_routing_rollback.sql
@@ -5,4 +5,7 @@ DROP TABLE IF EXISTS ai_private_model_profiles;
 DROP TABLE IF EXISTS ai_capability_route_audit;
 DROP TABLE IF EXISTS ai_capability_routes;
 
+DELETE FROM schema_migrations
+WHERE migration_id = '061_celar_ai_capability_routing';
+
 COMMIT;
